f17de35a593fbf5440d9967caa6f4c78981818cc
DongYoungSon <ehddud2978@gmail.com>
2019-12-18 16:35:15 +0900

    2 출력 삭제

--- src/main/java/dana/djp/ticketsite/App.java
+++ src/main/java/dana/djp/ticketsite/App.java
@@ -6,7 +6,6 @@ public class App {
   public static void main(String[] args) {
     System.out.println("세상 모든 공연의 집합소");
     System.out.println("1111");
-    System.out.println("2222");
     
   }
 }
