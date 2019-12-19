254ec494f2c3897d14d1facc99a962e58299b4a4
DongYoungSon <ehddud2978@gmail.com>
2019-12-19 18:54:09 +0900

    v02

--- src/main/java/dana/djp/ticketsite/App.java
+++ src/main/java/dana/djp/ticketsite/App.java
@@ -7,47 +7,75 @@ public class App {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     
-    System.out.print("장르: ");
-    String genre = keyboard.nextLine();
-        
-    System.out.print("공연명: ");
-    String name = keyboard.nextLine();
+    class Infor {
+      String genre;
+      String name;
+      String location;
+      Date startDate;
+      Date endDate;
+      int runnigTime;
+      int age;
+      String introduce;
+    }
     
-    System.out.print("장소: ");
-    String location = keyboard.nextLine();
+    String response;
     
-    System.out.print("공연시작: ");
-    Date startdate = Date.valueOf(keyboard.next());
+    final int Size = 100;
+    Infor[] infos = new Infor[Size];
     
-    System.out.print("공연종료: ");
-    Date enddate = Date.valueOf(keyboard.next());
     
-    System.out.print("러닝타임: ");
-    int runningtime = keyboard.nextInt();
-    
-    System.out.print("관람연령: ");
-    int age = keyboard.nextInt();
-    
-    keyboard.nextLine();
-    
-    System.out.print("공연소개: ");
-    String introduce = keyboard.nextLine();
-    
-    System.out.println();
-    
-    System.out.printf("장르: %s\n", genre);
-    System.out.printf("공연명: %s\n", name);
-    System.out.printf("장소: %s\n", location);
-    System.out.printf("공연기간: %s ~ %s 분\n", startdate, enddate);
-    System.out.printf("러닝타임: %d 시간\n ", runningtime);
-    System.out.printf("관람연령: 만 %d세 이상\n", age);
-    System.out.printf("공연소개: %s\n", introduce);
-    
+    int count = 0;
+    for(int i = 0; i < Size; i++) {
+      Infor info = new Infor();
+      
+      count++;
+
+      System.out.print("장르: ");
+      info.genre = keyboard.nextLine();
+          
+      System.out.print("공연명: ");
+      info.name = keyboard.nextLine();
+      
+      System.out.print("장소: ");
+      info.location = keyboard.nextLine();
+      
+      System.out.print("공연시작: ");
+      info.startDate = Date.valueOf(keyboard.next());
+      
+      System.out.print("공연종료: ");
+      info.endDate = Date.valueOf(keyboard.next());
+      
+      System.out.print("러닝타임: ");
+      info.runnigTime = keyboard.nextInt();
+      
+      System.out.print("관람연령: ");
+      info.age = keyboard.nextInt();
+      
+      keyboard.nextLine();
+      
+      System.out.print("공연소개: ");
+      info.introduce = keyboard.nextLine();
+      
+      infos[i] = info;
+      
+      System.out.print("계속 작성하시겠습니까? (Y/n)");
+      response = keyboard.nextLine();
+      if (!response.equalsIgnoreCase("y")) {
+        break;
+      }
+      
+      
+      
+    }
+   
     keyboard.close();
     
-    
-    
-    
-    
+    for (int i = 0; i < count; i++) {
+      Infor info = infos[i];
+      System.out.printf("장르: %s\n공연명: %s\n장소: %s\n공연기간: %s ~ %s 분\n"
+          + "러닝타임: %d시간\n관람연령: 만 %d세 이상\n공연소개: %s\n", 
+          info.genre, info.name, info.location, info.startDate, info.endDate, info.runnigTime, info.age, info.introduce);
+      
+    }
   }
 }
