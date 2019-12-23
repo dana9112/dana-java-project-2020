bdd6596f3b41dbd6e8351f8785d2b652a5809fc4
DongYoungSon <ehddud2978@gmail.com>
2019-12-23 17:13:55 +0900

    v04-리터럴과 콘솔 출력 다루기

--- src/main/java/dana/djp/ticketsite/App.java
+++ src/main/java/dana/djp/ticketsite/App.java
@@ -1,38 +1,52 @@
 package dana.djp.ticketsite;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-  
+    
     Scanner keyboard = new Scanner(System.in);
-    String command;
     
-    do {
-      System.out.print("명령> ");
-      command = keyboard.nextLine();
-      
-      switch (command) {
-        case "/infor/add":
-          
-          break;
-          
-        case "/infor/list":
-          break;
-          
-        case "/audience/add":
-          break;
-          
-         
-      }
-      
-    } while (!command.equalsIgnoreCase("quit"));
+    System.out.print("번호: ");
+    int no = keyboard.nextInt();
+    keyboard.nextLine();
     
-    System.out.println("완료");
+    System.out.print("장르: ");
+    String genre = keyboard.nextLine();
     
+    System.out.print("공연명: ");
+    String name = keyboard.nextLine();
     
+    System.out.print("장소: ");
+    String location = keyboard.nextLine();
+    
+    System.out.print("공연시작: ");
+    Date startDate = Date.valueOf(keyboard.next());
+    
+    System.out.print("공연종료: ");
+    Date endDate = Date.valueOf(keyboard.next());
+    
+    System.out.print("러닝타임: ");
+    int runningTime = keyboard.nextInt();
+    
+    System.out.print("관람연령: ");
+    int age = keyboard.nextInt();
+    
+    System.out.println();
+    keyboard.nextLine();
+    
+    System.out.printf("번호: %d\n", no);
+    System.out.printf("장르: %s\n", genre);
+    System.out.printf("공연명: %s\n", name);
+    System.out.printf("장소: %s\n", location);
+    System.out.printf("공연기간: %s ~ %s\n", startDate, endDate);
+    System.out.printf("러닝타임: %d 시간\n", runningTime);
+    System.out.printf("관람연령: 만 %d 이상\n", age);
     
     keyboard.close();
-    } // main() end
-  } // class() end
+    
+
+  } // main() end
+} // class() end
 
